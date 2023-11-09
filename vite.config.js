99374496ae6bc38bea8686ecea8e7310e169cf34
Naya Naya <naya252naya@gmail.com>
2023-11-10 02:33:32 +0300

    feat: add eslint and prettier

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import path from 'path'
-import {resolve} from 'path'
+import path, { resolve } from 'path';
 
 export default {
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src/assets')
-        }
+  plugins: [],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/assets'),
     },
-    build: {
-        css: {
-            devSourcemap: true // enable CSS source maps during development
-        },
-        sourcemap: true, // enable production source maps
-        input: {
-            main: resolve(__dirname, 'index.html'),
-            menu: resolve(__dirname, 'menu/index.html'),
-        }
-    }
-}
+  },
+  build: {
+    css: {
+      devSourcemap: true, // enable CSS source maps during development
+    },
+    sourcemap: true, // enable production source maps
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu/index.html'),
+      },
+    },
+  },
+};
