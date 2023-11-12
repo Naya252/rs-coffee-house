bd8cb8be04a5b310b7785702b15227ee5a087a8e
Anna Schavelkova <naya252naya@gmail.com>
2023-11-12 21:44:35 +0300

    feat: add enjoy section

---
+++ src/ui/share/enjoy.js
@@ -0,0 +1,27 @@
+import '../../sass/enjoy.scss';
+import { CUP_ICON } from '../../core/constants';
+
+document.querySelector('#enjoy').innerHTML = `
+<div class="enjoy__content">
+  <video
+    autoplay
+    poster="./src/assets/video/img-hero.svg"
+    muted
+    loop>
+    <source
+      src="/src/assets/video/video (2160p).mp4"
+      type="video/mp4" />
+  </video>
+
+  <div class="enjoy__content_offer-wrapper">
+    <div class="enjoy__content_offer">
+      <h1 class="heading-1 mb-10"><em class="text-accent">Enjoy</em> premium coffee at our charming cafe</h1>
+      <p class="mb-10">
+        With its inviting atmosphere and delicious coffee options, the Coffee House Resource is a popular destination
+        for coffee lovers and those seeking a warm and inviting space to enjoy their favorite beverage.
+      </p>
+      <a class="button-primary" href="/menu/"> <span>Menu</span> ${CUP_ICON} </a>
+    </div>
+  </div>
+</div>
+`;
