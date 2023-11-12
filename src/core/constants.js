8fa332b4ce6bbdc3290694018710a8e79df12f9b
Anna Schavelkova <naya252naya@gmail.com>
2023-11-12 14:23:42 +0300

    feat: add constants

---
+++ src/core/constants.js
@@ -0,0 +1,15 @@
+import cupImg from './icons/coffee-cup';
+import twitterImg from './icons/twitter';
+import instagramImg from './icons/instagram';
+import facebookImg from './icons/facebook';
+import phoneImg from './icons/phone';
+import pinImg from './icons/pin-alt';
+import clockImg from './icons/clock';
+
+export const CUP_ICON = cupImg;
+export const TWITTER_ICON = twitterImg;
+export const INSTAGRAM_ICON = instagramImg;
+export const FACEBOOK_ICON = facebookImg;
+export const PHONE_ICON = phoneImg;
+export const PIN_ICON = pinImg;
+export const CLOCK_ICON = clockImg;
