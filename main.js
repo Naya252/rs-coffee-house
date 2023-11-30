213fd2bb7518ed477af1ad1b3cead19c469fec09
Naya Naya <naya252naya@gmail.com>
2023-12-01 00:57:50 +0300

    fix: change condition to check path

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import './src/sass/style.scss';
-import { BASE_URL, CURRENT_PATH } from './src/core/constants';
+import { CURRENT_PATH } from './src/core/constants';
 import createMainPage from './src/ui/views/mainPage';
 import createMenuPage from './src/ui/views/menuPage';
 
-if (BASE_URL === CURRENT_PATH) {
-  createMainPage();
-} else {
+if (CURRENT_PATH.includes('menu')) {
+  console.log('menu');
   createMenuPage();
+} else {
+  console.log('main');
+  createMainPage();
 }
