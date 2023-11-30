7533c5423c47c78c1d334b13d243ac7acfb651d4
Naya Naya <naya252naya@gmail.com>
2023-11-30 21:33:09 +0300

    refactor: delete sections from html pages

--- src/ui/sections/mobile.js
+++ src/ui/sections/mobile.js
@@ -2,32 +2,34 @@ import '../../sass/layouts/mobile.scss';
 import '../../sass/components/button.scss';
 import { APPLE_ICON, GOOGLE_PLAY_ICON } from '../../core/constants';
 
-document.querySelector('#mobile-app').insertAdjacentHTML(
-  'afterbegin',
+document.querySelector('#about').insertAdjacentHTML(
+  'afterend',
   `
-<div class="mobile-app__offer">
-  <h2 class="heading-2"><em class="text-accent">Download</em> our apps to start ordering</h2>
-  <p>
-    Download the Resource app today and experience the comfort of ordering your favorite coffee from wherever you are
-  </p>
-  <div class="mobile-app__offer_buttons">
+<section id="mobile-app" class="section">
+  <div class="mobile-app__offer">
+    <h2 class="heading-2"><em class="text-accent">Download</em> our apps to start ordering</h2>
+    <p>
+      Download the Resource app today and experience the comfort of ordering your favorite coffee from wherever you are
+    </p>
+    <div class="mobile-app__offer_buttons">
+      <button class="button-primary mobile-app-btn transparent-dark-btn svg-fill">
+      ${APPLE_ICON}
+      <span class="button__text">
+        <span class="caption">Available on the</span>
+        <span>App Store</span>
+      </span>
+    </button>
     <button class="button-primary mobile-app-btn transparent-dark-btn svg-fill">
-    ${APPLE_ICON}
-    <span class="button__text">
-      <span class="caption">Available on the</span>
-      <span>App Store</span>
-    </span>
-  </button>
-  <button class="button-primary mobile-app-btn transparent-dark-btn svg-fill">
-  ${GOOGLE_PLAY_ICON}
-    <span class="button__text">
-      <span class="caption">Available on</span>
-      <span>Google Play</span>
-    </span>
-  </button>
+    ${GOOGLE_PLAY_ICON}
+      <span class="button__text">
+        <span class="caption">Available on</span>
+        <span>Google Play</span>
+      </span>
+    </button>
+    </div>
   </div>
-</div>
-<div class="mobile-app__img">
-</div>
+  <div class="mobile-app__img">
+  </div>
+</section>
 `,
 );
