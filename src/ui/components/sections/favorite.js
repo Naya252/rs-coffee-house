aeb62ab8ba2e77f7124fa416e3a7f4b6b13c57f7
Naya252 <naya252naya@gmail.com>
2023-12-09 02:53:13 +0300

    feat: add all html slides

--- src/ui/components/sections/favorite.js
+++ src/ui/components/sections/favorite.js
@@ -1,23 +1,39 @@
 import '../../../sass/layouts/_favorite.module.scss';
 import '../../../sass/components/_button.module.scss';
 import { ARROW_LEFT_ICON, ARROW_RIGHT_ICON } from '../../../share/constants';
+import { slider, setupSlider } from '../../../services/sliderService';
+
+function createSlider() {
+  let slidesHtml = ``;
+  slider.items.forEach((el) => {
+    slidesHtml += `
+    <div class="slide-wrapper" id="${el.id}-slide-wrapper">
+      <div id="${el.id}-slide" class="slider__content mx-auto">
+        <div class="slider__content_img"></div>
+        <div class="slider__content_text mt-5">
+          <h3 class="heading-3">${el.name}</h3>
+          <p>
+            ${el.description}
+          </p>
+          <h3 class="heading-3">$${el.price}</h3>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+  return slidesHtml;
+}
 
 export default function createFavoriteSection() {
+  const slidesHtml = createSlider();
   document.querySelector('#enjoy').insertAdjacentHTML(
     'afterend',
     `
   <section id="favorite-coffee" class="section">
     <h2 class="heading-2 mb-10">Choose your <em class="text-accent">favorite</em> coffee</h2>
-    <div class="slider">
-      <div class="slider__content mx-auto">
-        <div class="slider__content_img"></div>
-        <div class="slider__content_text mt-5">
-          <h3 class="heading-3">S’mores Frappuccino</h3>
-          <p>
-            This new drink takes an espresso and mixes it with brown sugar and cinnamon before being topped with oat milk.
-          </p>
-          <h3 class="heading-3">$5.50</h3>
-        </div>
+    <div class="slider" id="slider">
+    <div id="slider-wrapper">
+      ${slidesHtml}
       </div>
       <div class="slider__buttons">
         <button class="slider__buttons_btn-left rounded-btn transparent-dark-btn">${ARROW_LEFT_ICON}</button>
@@ -32,4 +48,6 @@ export default function createFavoriteSection() {
   </section>
   `,
   );
+
+  setupSlider(document.querySelector('.slider'));
 }
